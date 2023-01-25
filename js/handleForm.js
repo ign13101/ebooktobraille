64afffdecf863a9f07ae889342879e806ba7f8f9
ign13101 <nacho-13101@hotmail.com>
2023-01-25 14:15:35 +0100

    Modified parsing of elements inside xhtml and html files

--- js/handleForm.js
+++ js/handleForm.js
@@ -1,12 +1,19 @@
 "use strict";
 
 function main() {
+
     console.log("Start");
     let registerForm = document.getElementById("form-ebook-upload");
     registerForm.onsubmit = handleSubmitFile;
 }
 
+// TO DO: reformat this into different functions, maybe different files
+// TO DO: add meaningful coments
+// TO DO: check if EPUBv3 can have html files, and consider using the same functions for both
+// TO DO: check if the variable types used are optimal
+
 function handleSubmitFile(event) {
+
     event.preventDefault();
     const zip = new JSZip();
     let form = event.target;
@@ -17,8 +24,6 @@ function handleSubmitFile(event) {
         unzippedFile.forEach(function (relativePath, zipEntry) {
             if (zipEntry.name.endsWith(".opf")) {
                 unzippedFile.file(zipEntry.name).async("string").then(function (content) {
-                    // let readableContent = parseXhtml(content);
-                    // console.log(content);
                     const parser = new DOMParser();
                     const opfDoc = parser.parseFromString(content, "text/xml");
                     console.log(opfDoc.querySelector("package"));
@@ -31,7 +36,6 @@ function handleSubmitFile(event) {
                     for (const item of spine.querySelectorAll("itemref")) {
                         spineIds.push(item.getAttribute("idref"));
                     }
-                    // console.log(spineIds);
                     const hrefs = []
                     for (const id of spineIds) {
                         hrefs.push(manifest.querySelector('item[id=' + id + ']').getAttribute("href"));
@@ -39,9 +43,7 @@ function handleSubmitFile(event) {
                     console.log(hrefs);
                     switch (version) {
                         case 3:
-                            // parseEpub3(unzippedFile)
                             for (const href of hrefs) {
-                                // unzipFile.filter(filename => filename.contains(href));
                                 unzipFile.forEach(function (relativePath, zipEntry) {
                                     if (zipEntry.name.includes(href)) {
                                         if (zipEntry.name.endsWith(".xhtml") || zipEntry.name.endsWith(".html")) {
@@ -55,30 +57,39 @@ function handleSubmitFile(event) {
                                     }
                                 });
                             }
-
                             break;
+
                         case 2:
                             for (const href of hrefs) {
-                                // unzipFile.filter(filename => filename.contains(href));
                                 unzipFile.forEach(function (relativePath, zipEntry) {
                                     if (zipEntry.name.includes(href)) {
-                                        if (zipEntry.name.endsWith(".xhtml") || zipEntry.name.endsWith(".html")) {
+                                        if (zipEntry.name.endsWith(".xhtml")) {
                                             console.log(zipEntry.name);
                                             unzipFile.file(zipEntry.name).async("string").then(function (content) {
                                                 let readableContent = parseEpubV3(content);
-                                                console.log(readableContent)
-                                                // console.log(readableContent);
+                                                // check if it contains any character different from whitespace
+                                                if (/\S/.test(readableContent)) {
+                                                    console.log(readableContent);
+                                                }
+                                            });
+                                        }
+                                        if (zipEntry.name.endsWith(".html")) {
+                                            console.log(zipEntry.name);
+                                            unzipFile.file(zipEntry.name).async("string").then(function (content) {
+                                                let readableContent = parseEpubV2(content);
+                                                // check if it contains any character different from whitespace
+                                                if (/\S/.test(readableContent)) {
+                                                    console.log(readableContent);
+                                                }
                                             });
                                         }
                                     }
                                 });
                             }
-
                             break;
                         default:
                             break;
                     }
-
                 });
             };
         });
@@ -89,10 +100,25 @@ function handleSubmitFile(event) {
 };
 
 
+// you should consider excluding a tags, as they usually have no meaning in the book, only digital books 
+// whose purpose is to skip a couple of pages might use it
+
 function parseEpubV3(xhtmlText) {
     const parser = new DOMParser();
     const xhtmlDoc = parser.parseFromString(xhtmlText, "application/xhtml+xml");
-    const allElements = xhtmlDoc.getElementsByTagName("*");
+    const allElements = xhtmlDoc.body.getElementsByTagName("*");
+    let readableText = "";
+    for (let i = 0; i < allElements.length; i++) {
+        readableText += allElements[i].textContent + " ";
+    }
+    return readableText;
+
+}
+
+function parseEpubV2(htmlText) {
+    const parser = new DOMParser();
+    const htmlDoc = parser.parseFromString(htmlText, "application/xhtml+xml");
+    const allElements = htmlDoc.body.getElementsByTagName("*");
     let readableText = "";
     for (let i = 0; i < allElements.length; i++) {
         readableText += allElements[i].textContent + " ";
